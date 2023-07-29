59dde200f535d0e49092cab3dc84cff24ba51c8a
Cyrus Mobini <miladmobini2@gmail.com>
2023-07-29 16:03:57 -0400

    Dynamic City scene parsing with floatable/clickable objects

--- src/experience/world/City.tsx
+++ src/experience/world/City.tsx
@@ -1,28 +1,82 @@
+import { Float, useGLTF } from "@react-three/drei";
 import { RigidBody } from "@react-three/rapier";
-import BrokenDownCity from "./BrokenDownCity";
+import { ReactNode } from "react";
+import { MODELS } from "../utils/constants";
+
+const onPointerHandlers: { [name: string]: (e: any) => void } = {
+  Suzanne: (e: Event) => {
+    e.stopPropagation();
+    console.log("Suzanne clicked", e);
+  },
+  blockEvents: (e: Event) => e.stopPropagation(),
+};
 
 function City() {
-  const onPointerHandlers = {
-    onSuzanne: (e: any) => {
-      e.stopPropagation();
-      console.log("Suzanne clicked", e);
-    },
-    blockEvents: (e: any) => e.stopPropagation(),
-  };
+  const { scene } = useGLTF(MODELS.CITY);
+  const cityScene = scene.clone();
+  const children = cityScene.children;
+  cityScene.children = [];
+
+  const clickableBlocks: ReactNode[] = [];
+  children.forEach((child: any) => {
+    if (child.type === "Mesh") {
+      child.castShadow = true;
+      child.receiveShadow = true;
+      child.material.envMapIntensity = 3;
+      if (onPointerHandlers[child.name]) {
+        if (child.name.includes("floating")) {
+          clickableBlocks.push(
+            <Float
+              floatingRange={[-0.05, 0.05]}
+              rotationIntensity={0.3}
+              key={child.name}
+            >
+              <mesh
+                key={child.name}
+                name={child.name}
+                castShadow
+                receiveShadow
+                geometry={child.geometry}
+                material={child.material}
+                onPointerDown={onPointerHandlers[child.name]}
+              />
+            </Float>
+          );
+        } else {
+          clickableBlocks.push(
+            <mesh
+              key={child.name}
+              name={child.name}
+              castShadow
+              receiveShadow
+              geometry={child.geometry}
+              material={child.material}
+              onPointerDown={onPointerHandlers[child.name]}
+            />
+          );
+        }
+        return;
+      }
+    }
+    cityScene.children.push(child);
+  });
 
   return (
-    <RigidBody
-      type="fixed"
-      colliders="trimesh"
-      friction={0.5}
-      restitution={0.2}
-      position={[0, -0.5, 0]}
-      scale={[50, 50, 50]}
-    >
-      <group dispose={null} scale={0.01}>
-        <BrokenDownCity onPointerHandlers={onPointerHandlers} />
-      </group>
-    </RigidBody>
+    <>
+      <RigidBody
+        type="fixed"
+        colliders="trimesh"
+        friction={0.5}
+        restitution={0.2}
+        position={[0, -0.5, 0]}
+        scale={[50, 50, 50]}
+      >
+        <group onPointerDown={onPointerHandlers.blockEvents}>
+          <primitive object={cityScene} scale={0.01} />
+        </group>
+        <group scale={0.01}>{clickableBlocks}</group>
+      </RigidBody>
+    </>
   );
 }
 
