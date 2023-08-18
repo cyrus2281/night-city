4b00f453f23e44af7c922163802d996229f35414
Cyrus Mobini <miladmobini2@gmail.com>
2023-08-18 00:49:42 -0400

    Added Stats View template/UI

--- src/experience/interface/ui/InterfaceButtons.tsx
+++ src/experience/interface/ui/InterfaceButtons.tsx
@@ -4,6 +4,7 @@ import "./InterfaceButtons.scss";
 import Icon from "../../../components/Icon";
 import MuteButton from "./MuteButton";
 import { PAGES, openGithub, openLinkedIn } from "../../utils/constants";
+import StatsView from "./StatsView";
 
 function InterfaceButtons() {
   const navigate = useNavigate();
@@ -11,7 +12,7 @@ function InterfaceButtons() {
   return (
     <div className="interface-buttons">
       <div className="interface-buttons-top">
-        <div className="interface-buttons-top-left">
+        <div className="interface-buttons-top-row">
           <Button
             theme="dark"
             neon
@@ -21,50 +22,55 @@ function InterfaceButtons() {
           >
             Resume
           </Button>
-          <span className="status-label">7/10</span>
+          <div className="interface-buttons-top-group">
+            <Button
+              neon
+              circle
+              className="small-padding"
+              onClick={openLinkedIn}
+            >
+              <Icon>linkedin</Icon>
+            </Button>
+            <Button neon circle className="small-padding" onClick={openGithub}>
+              <Icon>github</Icon>
+            </Button>
+            <Button
+              neon
+              circle
+              onClick={() => {
+                navigate(PAGES.CONTACT, { replace: true });
+              }}
+            >
+              <Icon>email</Icon>
+            </Button>
+            <Button
+              neon
+              circle
+              onClick={() => {
+                navigate(PAGES.CREDITS, { replace: true });
+              }}
+            >
+              <Icon>info</Icon>
+            </Button>
+            <MuteButton />
+          </div>
         </div>
-        <div className="interface-buttons-top-right">
-          <Button neon circle className="small-padding"  onClick={openLinkedIn}>
-            <Icon>linkedin</Icon>
-          </Button>
-          <Button neon circle className="small-padding" onClick={openGithub}>
-            <Icon>github</Icon>
-          </Button>
-          <Button
-            neon
-            circle
-            onClick={() => {
-              navigate(PAGES.CONTACT, { replace: true });
-            }}
-          >
-            <Icon>email</Icon>
-          </Button>
-          <Button
-            neon
-            circle
-            onClick={() => {
-              navigate(PAGES.CREDITS, { replace: true });
-            }}
-          >
-            <Icon>info</Icon>
-          </Button>
-          <MuteButton />
+        <div className="interface-buttons-top-row">
+          <StatsView />
         </div>
       </div>
       <div className="interface-buttons-bottom">
-        <div className="interface-buttons-bottom-left">
-          <Button
-            circle
-            uppercase
-            theme="dark"
-            neon
-            resizable
-            onClick={() => window.jump && window.jump()}
-            onTouchStart={() => window.jump && window.jump()}
-          >
-            jump
-          </Button>
-        </div>
+        <Button
+          circle
+          uppercase
+          theme="dark"
+          neon
+          resizable
+          onClick={() => window.jump && window.jump()}
+          onTouchStart={() => window.jump && window.jump()}
+        >
+          jump
+        </Button>
       </div>
     </div>
   );
