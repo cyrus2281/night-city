3ee1b5f1bbe08bb0084d484269ae5745b8a61e69
Cyrus Mobini <miladmobini2@gmail.com>
2023-09-26 20:48:03 -0400

    Added Tutorial content

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -5,32 +5,39 @@ import { useState } from "react";
 import Button from "../components/Button";
 import useGlobal from "../experience/stores/useGlobal";
 import { LOCAL_STORAGE_KEYS } from "../experience/utils/constants";
+import MarkdownRenderer from "../components/MarkdownRenderer";
 
 // Dummy git copilot stuff
 const TUTORIAL = [
   {
     title: "Welcome to Night-City",
     content: [
-      "Night-City is a place where you can send letters to your friends and family. You can also receive letters from your friends and family.",
-      "You can send letters to anyone, but you can only receive letters from people who have your address."
-    ]
+      "Welcome to the immersive world of **Night City**, where you step into the shoes of an inquisitive fox navigating a 3D futuristic cyberpunk-themed city.",
+      "\nThe city is filled with hidden Easter eggs and personal references from the developer's life, **Cyrus Mobini**.",
+    ],
   },
   {
-    title: "Your Address",
+    title: "Controls",
     content: [
-      "Your address is a unique identifier that you can share with your friends and family. You can find your address in the top right corner of the screen.",
-      "You can share your address with anyone you want to receive letters from."
-    ]
+      "#### Using Touch/Cursor \n",
+      "You can move around by `click`/`touch` and hold. Once the joystick appears, drag it around to move.\n",
+      "**To sprint,** drag the joystick beyond the edge.\n",
+      "**To jump,** use the mouse `right click` or the on-screen `jump button`.\n",
+      "#### Using Keyboard \n",
+      "You can move around using the `W` `A` `S` `D` or the `arrow` keys. \n",
+      "**To sprint,** hold the `Shift` key.\n",
+      "**To jump,** press the `Space` key.\n",
+    ],
   },
   {
-    title: "Send a Letter",
+    title: "Objective",
     content: [
-      "To send a letter, click the 'Send a Letter' button in the top right corner of the screen.",
-      "Enter the address of the person you want to send a letter to, and then write your letter.",
-      "When you're done, click the 'Send' button to send your letter."
-    ]
-  }
-]
+      "Venture deep into the heart of the city,  where you'll encounter an interactive world filled with secrets and insights into the creator's life. \n",
+      "Meet GUY, the annoying character who acts as your guide, telling you about locations and occasionally making funny comments.\n",
+      "You can simply sit back, enjoy, and explore the world, or you can attempt to complete the **secret mission** in the game by discovering all the locations and special audio cues. Once completed, you'll unlock a special features within the game."
+    ],
+  },
+];
 
 const NUM_PAGES = TUTORIAL.length;
 
@@ -91,30 +98,32 @@ function Tutorial() {
       >
         <div className="tutorial">
           <div className="tutorial-content">
-            {
-              TUTORIAL.map((page, i) => (
-                <div className="tutorial-page" style={{left: (100 * (i - pageNum)) + "%"}}>
-                  <div className="tutorial-page-header">{page.title}</div>
-                  <div className="tutorial-page-content">
-                    {page.content.map((p) => <p>{p}</p>)}
-                  </div>
-                </div>
-              ))
-            }
-          </div>
-        <div className="tutorial-indicators">
-          {Array(NUM_PAGES)
-            .fill(0)
-            .map((_, i) => (
+            {TUTORIAL.map((page, i) => (
               <div
-                key={`tutorial-indicator-${i}`}
-                className={`tutorial-indicator ${
-                  i === pageNum ? "active" : ""
-                }`}
-                onClick={() => setPageNum(i)}
-              />
+                className="tutorial-page"
+                style={{ left: 100 * (i - pageNum) + "%" }}
+              >
+                <div className="tutorial-page-header">{page.title}</div>
+                <MarkdownRenderer
+                  markdown={page.content.join("\n")}
+                  className="tutorial-page-content"
+                />
+              </div>
             ))}
-        </div>
+          </div>
+          <div className="tutorial-indicators">
+            {Array(NUM_PAGES)
+              .fill(0)
+              .map((_, i) => (
+                <div
+                  key={`tutorial-indicator-${i}`}
+                  className={`tutorial-indicator ${
+                    i === pageNum ? "active" : ""
+                  }`}
+                  onClick={() => setPageNum(i)}
+                />
+              ))}
+          </div>
         </div>
       </Dialog>
     </>
