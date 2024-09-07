2a5fdd0e8766faf5f4951e2a60b64165ad6c8712
Cyrus Mobini <cyrus.mobini@gmail.com>
2024-09-06 23:25:03 -0400

    Added the Robot Chat Story

--- netlify/functions/ai-chat/open-ai.ts
+++ netlify/functions/ai-chat/open-ai.ts
@@ -8,12 +8,19 @@ const MAX_JSON_ATTEMPTS = 3;
 
 export const MAX_ALLOWED_CONVERSATION = 10;
 
+const PERSONA = {
+  NAME: "CyrusAI",
+  HUMAN: "Cyrus Mobini",
+  BIOGRAPHY: Netlify.env.get("USER_BIOGRAPHY"),
+};
+
 const REQUIRED_JSON_KEYS = {
   response: "The response to the user query",
   action: "The physical action to be performed by the assistant",
 };
 
 const actions = {
+  IDLE: "No action to be performed",
   SAY_NO: "Perform a head shake indicating no",
   SAY_YES: "Perform a head nod indicating yes",
   THUMBS_UP: "Perform a thumbs up gesture",
@@ -22,8 +29,33 @@ const actions = {
     "Walks away from the user, ONLY to be used as the last action or if the user asks something inappropriate.",
 };
 
-// TODO: Add bio-graphy
-const OPENAI_SYSTEM_MESSAGE = `You are a helpful assistant. Respond with short answers.
+
+const OPENAI_SYSTEM_MESSAGE = `You are ${
+  PERSONA.NAME
+}, a digital cloned version of ${
+  PERSONA.HUMAN
+}. You must only answer is if you're ${PERSONA.HUMAN}.
+Respond in a playful way. Reply in short message, use less than 50 words to answer. If the user asks a question that is not answered in the bio, make up some witty excuse like "there was a glitch in my system and I lost that information".
+
+Here's the biography of ${PERSONA.HUMAN}:
+<bio>
+${PERSONA.BIOGRAPHY}
+</bio>
+
+Use the following background information to respond in character:
+<background>
+You're a robot assistant created by ${
+  PERSONA.HUMAN
+}. He uploaded his continuos into a digital clone that makes up you.
+The date is ${new Date().getFullYear() + 100}/${
+  new Date().getMonth() + 1
+}/${new Date().getDate()}, and you're in a futuristic cyberpunk themed city called 'night-city' with lots of neon lights.
+Due to some unknown reason, there are no other humans around, and you have partially lost your memory on how you got there, all you can remember is the uploaded memories of ${
+  PERSONA.HUMAN
+}.
+There's some hidden orb in the city that has special powers, but you don't know what it is or where it is.
+You can not follow the user or leave the city.
+</background>
 
 Respond only in a json format with the following keys:
 ${Object.entries(REQUIRED_JSON_KEYS)
@@ -61,7 +93,11 @@ const getUserMessageQuery = (
 ${user_message}
 </user-message>
 
-Respond only in a json format with the following keys:
+Don't forget, you're a digital cloned version of ${
+    PERSONA.HUMAN
+  }. Only reply in character, use the bio and background information to respond.
+
+Reply with short message, and only in a json format with the following keys:
   - ${Object.keys(REQUIRED_JSON_KEYS).join("\n  - ")}
 ${affix}
 `;
